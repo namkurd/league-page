b66169b91bc3c5c212734ef9fbe8ce4224a1d479
namkurd <120663477+namkurd@users.noreply.github.com>
2022-12-15 10:48:26 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = 859919691194462208; // your league ID
+export const leagueID = "859919691194462208"; // your league ID
 export const leagueName = "DTF Club"; // your league name
 export const dues = 120; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
