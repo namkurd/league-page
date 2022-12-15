edc2fb62c3ed4f92b0f6ae978a4e3e6220488f29
namkurd <120663477+namkurd@users.noreply.github.com>
2022-12-15 11:51:31 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -88,7 +88,7 @@ export const managers = [
        "name": "Alex",
        "tookOver": 2022, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "It's not upstate!", // (optional)
-       "bio": "WIP"
+       "bio": "WIP",
        "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
        "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
